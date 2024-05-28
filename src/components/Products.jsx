43d070e06d896c9bf2a6ea0f3eec72b35c2d80ef
prio12 <maksudurprio.mp@gmail.com>
2024-05-29 04:13:14 +0600

    delete feature added

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,15 +1,22 @@
+import { useState } from "react";
 import SingleProduct from "./SingleProduct";
 
 // eslint-disable-next-line react/prop-types
 const Products = ({ data }) => {
+
+  const [products, setProducts] = useState(data);
+
+  const handleDeleteProduct = (id) => {
+    setProducts(data?.filter((product) => product.id !== id));
+  };
   return (
     <div className="my-8">
       <h1 className="text-3xl font-bold text-center">Available Products</h1>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-5 my-8">
         {
           // eslint-disable-next-line react/prop-types
-          data?.map((phone) => (
-            <SingleProduct key={phone.id} phone={phone} />
+          products?.map((phone) => (
+            <SingleProduct key={phone.id} phone={phone} onDelete={handleDeleteProduct} />
           ))
         }
       </div>
--- src/components/SingleProduct.jsx
+++ src/components/SingleProduct.jsx
@@ -1,10 +1,22 @@
 import { Link, useLocation } from "react-router-dom";
 
 // eslint-disable-next-line react/prop-types
-const SingleProduct = ({ phone }) => {
+const SingleProduct = ({ phone,onDelete }) => {
   // eslint-disable-next-line react/prop-types
   const { id, title, brand, price, description, image_url } = phone;
   const { pathname } = useLocation();
+
+  const handleDelete = async () => {
+    await fetch(`http://localhost:3000/phones/${id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        onDelete(id);
+      });
+  };
+
   return (
     <div className="card w-96 bg-base-100 shadow-xl">
       <figure>
@@ -25,7 +37,7 @@ const SingleProduct = ({ phone }) => {
               <button className="btn bg-green-600 text-white">
                 <Link to={`/editProduct/${id}`}>Edit</Link>
               </button>
-              <button className="btn bg-red-500">Delete</button>
+              <button onClick={handleDelete} className="btn bg-red-500">Delete</button>
             </div>
           )}
         </div>
